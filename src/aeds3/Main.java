e3ca351f016d2246387b168b283c7b3ea9f5b2e7
rcfc1985 <rodolfo.coelho.85@gmail.com>
2022-08-10 18:09:00 -0300

    Initial commit

---
+++ src/aeds3/Main.java
@@ -0,0 +1,18 @@
+package aeds3;
+
+class Main {
+	  public static void main(String[] args) {
+	    Graph g1 = new Graph(4);
+	    // System.out.println(g1.toString());
+	    g1.addEdge(0, 1, 3);
+	    g1.addEdge(1, 0, 3);
+	    g1.addEdge(0, 3, 4);
+	    g1.addEdge(3, 0, 4);
+	    g1.addEdge(3, 4, 2); // warning
+	    System.out.println(g1);
+	    System.out.println(g1.degree(0));
+	    System.out.println(g1.degree(1));
+	    System.out.println(g1.degree(2));
+	    System.out.println(g1.degree(3));
+	  }
+}
